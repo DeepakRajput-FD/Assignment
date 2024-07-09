7e6ceea73d931f7975d80b9cb7e831fde7b61cb7
DeepakRajput-FD <DeepakRajput-FD>
2024-07-09 20:27:12 +0530

    assessment

--- Design/src/App.jsx
+++ Design/src/App.jsx
@@ -1,11 +1,19 @@
 import React from 'react'
 import LoginPage from './LoginPage'
+import { Provider } from 'react-redux'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { store } from './app/store'
 
 export default function App() {
   return (
     <>
-
-    <LoginPage/>
+  <Provider store={store} >
+    <BrowserRouter>
+    <Routes>
+      <Route path='/' Component={LoginPage} ></Route>
+    </Routes>
+    </BrowserRouter>
+  </Provider>
 
     </>
   )
--- Design/src/LoginPage.jsx
+++ Design/src/LoginPage.jsx
@@ -1,32 +1,38 @@
 import { Add, AddCircle, DarkMode, LightMode, PanoramaPhotosphere } from '@mui/icons-material'
 import { Box, Button, IconButton, TextField, Typography } from '@mui/material'
 import React, { useState } from 'react'
+import { useDispatch, useSelector } from 'react-redux';
+import { changeTo } from './Features/Slice1';
 
 export default function LoginPage() {
   
   const [toggle,setToggle]=useState(false);
 
+  const dispatch=useDispatch();
+
+  const data=useSelector((state)=>{
+      console.log(
+        state.changeKey.toggle
+      );
+    return state.changeKey.toggle;
+  })
+  
   return (
-    <>
+    <>    
 
     {
-      !toggle
+      data
       ?
-  <IconButton onClick={()=>setToggle(!toggle)} ><DarkMode sx={{color:'white',fontSize:'50px'}} /></IconButton>
-    :
-    <IconButton onClick={()=>setToggle(!toggle)} ><LightMode sx={{color:'white',fontSize:'50px'}} /></IconButton>
+      <IconButton onClick={()=>setToggle(dispatch(changeTo(false)))} ><LightMode sx={{color:'white',fontSize:'50px'}} /></IconButton>
+      :
+      <IconButton onClick={()=>setToggle(dispatch(changeTo(true)))} ><DarkMode sx={{color:'white',fontSize:'50px'}} /></IconButton>
 
     }
-
-
-
-
     
-        <Box sx={{backdropFilter:'blur(5px)',height:'450px',margin:'0px auto',width:'30%',border:'2px solid white',borderRadius:'20px',display:'flex',flexDirection:'column',background:toggle?'black':''}} >
+        <Box sx={{backdropFilter:'blur(5px)',height:'450px',margin:'0px auto',width:'30%',border:'2px solid white',borderRadius:'20px',display:'flex',flexDirection:'column',background:data?'black':''}} >
 
-        <Typography sx={{padding:3,color:toggle ? 'white' :'black',display:'flex',gap:1,fontSize:'20px',alignItems:'center',}} > <PanoramaPhotosphere sx={{fontSize:'40px',color:toggle ? 'red':'white'}} /> Amazing Services</Typography>
-
-        <Typography variant='h5' sx={{textTransform:'capitalize',paddingLeft:3,color:toggle ?'white':'black'}} >
+        <Typography sx={{padding:3,color:data ? 'white' :'black',display:'flex',gap:1,fontSize:'20px',alignItems:'center',}} > <PanoramaPhotosphere sx={{fontSize:'40px',color:data ? 'red':'white'}} /> Amazing Services</Typography>
+        <Typography variant='h5' sx={{textTransform:'capitalize',paddingLeft:3,color:data ?'white':'black'}} >
           Log In
         </Typography>
         
@@ -36,9 +42,12 @@ export default function LoginPage() {
 
       <Button variant='contained' sx={{width:'50ch',margin:'10px auto',borderRadius:2,height:'50px'}} >Log in</Button>
 
-      <Typography variant='h5' sx={{textAlign:'center',color:toggle ? 'white':'black'}} >----- OR ----- </Typography>
+      <Typography variant='h5' sx={{textAlign:'center',color:data ? 'white':'black'}} >----- OR ----- </Typography>
 
       <Button  sx={{width:'50ch',margin:'10px auto',borderRadius:2,height:'50px',background:'rgb(16 185 129)',color:'white'}} startIcon={<AddCircle/>} >Create Account</Button>
+
+<h3 style={{color:'red'}}>{data}</h3>
+
 
         </Box>
 
